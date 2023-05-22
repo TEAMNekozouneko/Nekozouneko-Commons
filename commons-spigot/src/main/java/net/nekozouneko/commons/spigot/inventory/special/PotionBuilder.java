4e2a5c32237cfd142ec48efedd9ce5752ba82ec5
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-22 15:26:48 +0900

    ポーションビルダーにまだ追加してなかった機能とアノテーションを付与

--- commons-spigot/src/main/java/net/nekozouneko/commons/spigot/inventory/special/PotionBuilder.java
+++ commons-spigot/src/main/java/net/nekozouneko/commons/spigot/inventory/special/PotionBuilder.java
@@ -6,7 +6,12 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class PotionBuilder extends AbsItemStackBuilder<PotionBuilder, PotionMeta> {
 
@@ -34,22 +39,86 @@ public class PotionBuilder extends AbsItemStackBuilder<PotionBuilder, PotionMeta
         super(itemStack, itemMeta);
     }
 
-    public PotionBuilder basePotionData(PotionData data) {
+    /**
+     * @see PotionMeta#setBasePotionData(PotionData)
+     */
+    public @NotNull PotionBuilder basePotionData(@NotNull PotionData data) {
         meta.setBasePotionData(data);
         return this;
     }
 
-    public PotionData basePotionData() {
+    /**
+     * @see PotionMeta#getBasePotionData()
+     */
+    public @NotNull PotionData basePotionData() {
         return meta.getBasePotionData();
     }
 
-    public PotionBuilder color(Color color) {
+    /**
+     * @see PotionMeta#setColor(Color)
+     */
+    public @NotNull PotionBuilder color(@Nullable Color color) {
         meta.setColor(color);
         return this;
     }
 
-    public Color color() {
+    /**
+     * @see PotionMeta#getColor()
+     */
+    public @Nullable Color color() {
         return meta.getColor();
+    }
+
+    /**
+     * @see PotionMeta#hasColor()
+     */
+    public boolean hasColor() {
+        return meta.hasColor();
+    }
+
+    /**
+     * @see PotionMeta#addCustomEffect(PotionEffect, boolean)
+     */
+    public @NotNull PotionBuilder customEffect(@NotNull PotionEffect effect, boolean overwrite) {
+        meta.addCustomEffect(effect, overwrite);
+        return this;
+    }
+
+    /**
+     * @see PotionMeta#removeCustomEffect(PotionEffectType)
+     */
+    public @NotNull PotionBuilder removeCustomEffect(@NotNull PotionEffectType effectType) {
+        meta.removeCustomEffect(effectType);
+        return this;
+    }
+
+    /**
+     * @see PotionMeta#clearCustomEffects()
+     */
+    public @NotNull PotionBuilder clearCustomEffects() {
+        meta.clearCustomEffects();
+        return this;
+    }
+
+    /**
+     * @see PotionMeta#getCustomEffects()
+     */
+    public @NotNull List<PotionEffect> customEffects() {
+        return meta.getCustomEffects();
+    }
+
+    /**
+     * @see PotionMeta#hasCustomEffects()
+     */
+    public boolean hasCustomEffects() {
+        return meta.hasCustomEffects();
+    }
+
+    /**
+     * @see PotionMeta#hasCustomEffect(PotionEffectType)
+     */
+    public boolean hasCustomEffect(PotionEffectType effectType) {
+        return meta.hasCustomEffect(effectType);
     }
 
 }
